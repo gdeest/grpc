4a81e5120a6d915ff35dce82210f1f2c92e834a6
Gaël Deest <gael.deest@tweag.io>
2019-04-08 13:22:51 +0200

    Update port_platform.h

--- include/grpc/impl/codegen/port_platform.h
+++ include/grpc/impl/codegen/port_platform.h
@@ -39,15 +39,9 @@
 #define NOMINMAX
 #endif /* NOMINMAX */
 
-#ifndef _WIN32_WINNT
-#error \
-    "Please compile grpc with _WIN32_WINNT of at least 0x600 (aka Windows Vista)"
-#else /* !defined(_WIN32_WINNT) */
-#if (_WIN32_WINNT < 0x0600)
-#error \
-    "Please compile grpc with _WIN32_WINNT of at least 0x600 (aka Windows Vista)"
-#endif /* _WIN32_WINNT < 0x0600 */
-#endif /* defined(_WIN32_WINNT) */
+/* Force define Windows version to Windows 10 before including windows.h */
+#define WINVER 0x0A00
+#define _WIN32_WINNT 0x0A00
 
 #include <windows.h>
 
